77dbf3c0f7b307e9561d241194af96bdc4e800ba
quarterback-action <quarterback-no-reply@underarmour.com>
2025-05-30 23:35:33 +0200

    feature: add new-employee (onboarding) pages

---
+++ app/(app)/education/new-employee/page.tsx
@@ -0,0 +1,9 @@
+import NewEmployeePage from '@/components/pages/Education/NewEmploye/NewEmployeePage';
+
+export default function NewEmployee() {
+	return (
+		<div className={'w-full'}>
+			<NewEmployeePage />
+		</div>
+	);
+}
---
+++ app/(app)/education/new-employee/portal-structure/page.tsx
@@ -0,0 +1,9 @@
+import PortalStructurePage from '@/components/pages/Education/NewEmploye/PortalStructurePage';
+
+export default function PortalStructure() {
+	return (
+		<div className={'w-full'}>
+			<PortalStructurePage />
+		</div>
+	);
+}
